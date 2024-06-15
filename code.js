a877d7bb485b7bc872666cf9b3334d8cc6a4659a
hamimhossainm <hamim.dashboard@gmail.com>
2024-06-15 19:59:04 +0600

    some service section works done

--- code.js
+++ code.js
@@ -1,4 +1,32 @@
+// Navigation bar effects on scroll
 window.addEventListener("scroll",function(){
     let header = document.querySelector("header");
     header.classList.toggle("sticky",scrollY > 0);
 });
+
+// Services section - model
+let serviceModels = document.querySelector(".service-model");
+let learnMoreBtn = document.querySelector(".learn-more-btn");
+let modelCloseBtn = document.querySelector(".model-close-btn");
+
+let model = function(modelClick){
+    serviceModels[modelClick].classList.add("active");
+}
+
+learnMoreBtn.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        model(i);
+    });
+});
+
+modelCloseBtn.forEach((modelCloseBtn) => {
+    modelCloseBtn.addEventListener("click", () => {
+        serviceModels.forEach((modelView) => {
+            modelView.classList.remove("active");
+        });
+    });
+});
+
+
+
+
