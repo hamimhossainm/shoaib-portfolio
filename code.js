d65b76092f9b32e204ce36e17262a58d05c67f7e
hamimhossainm <hamim.dashboard@gmail.com>
2024-06-29 05:31:43 +0600

    Project section works done

--- code.js
+++ code.js
@@ -1,32 +1,61 @@
 // Navigation bar effects on scroll
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
-    header.classList.toggle("sticky",scrollY > 0);
+    header.classList.toggle("sticky", scrollY > 0);
 });
 
 // Services section - model
-let serviceModels = document.querySelector(".service-model");
-let learnMoreBtn = document.querySelector(".learn-more-btn");
-let modelCloseBtn = document.querySelector(".model-close-btn");
+let serviceModels = document.querySelectorAll(".service-model");
+let learnMoreBtn = document.querySelectorAll(".learn-more-btn");
+let modelCloseBtn = document.querySelectorAll(".model-close-btn");
 
-let model = function(modelClick){
+
+let model = function(modelClick) {
     serviceModels[modelClick].classList.add("active");
 }
 
-learnMoreBtn.forEach((learnMoreBtn, i) => {
-    learnMoreBtn.addEventListener("click", () => {
+
+learnMoreBtn.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
         model(i);
     });
 });
 
-modelCloseBtn.forEach((modelCloseBtn) => {
-    modelCloseBtn.addEventListener("click", () => {
+
+modelCloseBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
         serviceModels.forEach((modelView) => {
             modelView.classList.remove("active");
         });
     });
 });
 
+
+// Project Section - model
+let projectModels = document.querySelectorAll(".project-model");
+let imgCards = document.querySelectorAll(".img-card");
+let projectCloseBtn = document.querySelectorAll(".project-close-btn");
+
+
+let projectModel = function(modelClick) {
+    projectModels[modelClick].classList.add("active");
+}
+
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        model(i);
+    });
+});
+
+
+projectCloseBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        projectModels.forEach((projectmodelView) => {
+            projectmodelView.classList.remove("active");
+        });
+    });
+});
 
 
 
